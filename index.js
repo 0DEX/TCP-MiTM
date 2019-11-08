1e9e488fc2f9e52981f7ee7360778bd9f44b18f0
Gerson Victor <gerson.vfs@gmail.com>
2019-11-07 21:19:29 -0300

    agora vai

--- index.js
+++ index.js
@@ -10,40 +10,39 @@ open.then((conn) => {
   return ch.assertQueue(q).then((ok) => {
     const data = "##,imei:000000000000000,A;";
     ch.sendToQueue(q, Buffer.from(data));
+
+    const server = net.createServer((socket) => {
+      const client = new net.Socket();
+
+      client.connect(5001, "10.158.0.6", () => {
+        console.log("client connected!");
+      });
+
+      client.on("data", (data) => {
+        console.log(data.toString());
+        socket.write(data);
+      });
+
+      socket.on("data", (data) => {
+        console.log(data.toString());
+        client.write(data);
+
+      });
+
+      client.on("end", () => {
+        console.log("client disconnected!");
+        socket.end();
+      });
+
+      socket.on("end", () => {
+        client.end();
+      });
+    }).on("error", (err) => {
+      throw err;
+    });
+
+    server.listen(5000, () => {
+      console.log("opened server on", server.port);
+    });
   });
-});
-
-
-const server = net.createServer((socket) => {
-  const client = new net.Socket();
-
-  client.connect(5001, "10.158.0.6", () => {
-    console.log("client connected!");
-  });
-
-  client.on("data", (data) => {
-    console.log(data.toString());
-    socket.write(data);
-  });
-
-  socket.on("data", (data) => {
-    console.log(data.toString());
-    client.write(data);
-
-  });
-
-  client.on("end", () => {
-    console.log("client disconnected!");
-    socket.end();
-  });
-
-  socket.on("end", () => {
-    client.end();
-  });
-}).on("error", (err) => {
-  throw err;
-});
-
-server.listen(5000, () => {
-  console.log("opened server on", server.port);
 });
