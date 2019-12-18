7757423ea387cbace2acd8b23d5425bf5fd7bef5
Gerson Víctor <gerson.vfs@gmail.com>
2019-12-17 21:55:17 -0300

    removed rabbit connection

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const net = require("net");
-const open = require("amqplib").connect(process.env.AMQP_URI);
+/* const open = require("amqplib").connect(process.env.AMQP_URI);
 
 const q = "tasks";
 
@@ -9,38 +9,40 @@ open.then((conn) => {
 }).then((ch) => {
   return ch.assertQueue(q).then((ok) => { 
 
-    const server = net.createServer((socket) => {
-      const client = new net.Socket();
-
-      client.connect(process.env.TARGET_PORT, process.env.TARGET_HOST, () => {
-        console.log("client connected!");
-      });
-
-      client.on("data", (data) => {
-        console.log(data.toString());
-        socket.write(data);
-      });
-
-      socket.on("data", (data) => {
-        console.log(data.toString());
-        client.write(data);
-        ch.sendToQueue(q, data);
-      });
-
-      client.on("end", () => {
-        console.log("client disconnected!");
-        socket.end();
-      });
-
-      socket.on("end", () => {
-        client.end();
-      });
-    }).on("error", (err) => {
-      throw err;
-    });
-
-    server.listen(process.env.APP_PORT, () => {
-      console.log(`Server running on port ${process.env.APP_PORT}.`);
-    });
+    
   });
+}); */
+
+const server = net.createServer((socket) => {
+  const client = new net.Socket();
+
+  client.connect(process.env.TARGET_PORT, process.env.TARGET_HOST, () => {
+    console.log("client connected!");
+  });
+
+  client.on("data", (data) => {
+    console.log(data.toString());
+    socket.write(data);
+  });
+
+  socket.on("data", (data) => {
+    console.log(data.toString());
+    client.write(data);
+    // ch.sendToQueue(q, data);
+  });
+
+  client.on("end", () => {
+    console.log("client disconnected!");
+    socket.end();
+  });
+
+  socket.on("end", () => {
+    client.end();
+  });
+}).on("error", (err) => {
+  throw err;
+});
+
+server.listen(process.env.APP_PORT, () => {
+  console.log(`Server running on port ${process.env.APP_PORT}.`);
 });
